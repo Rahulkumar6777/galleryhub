a3cd26ad794a834c9ed5dbf2d09972e54deab607
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-12 01:17:19 +0530

    make routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,10 @@
 import app from "./index.js"
 
+// routes
+import adminRoutes from './src/routes/admin.Routes.js'
+
+
+app.use('/api/v1/admin', adminRoutes)
 
 app.listen(process.env.PORT , '0.0.0.0' , () => {
     console.log(`server is running at port ${process.env.PORT}`)
