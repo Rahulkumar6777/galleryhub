4086e3066e8c0554c391e313a92a272e3f3095fc
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-12 01:17:09 +0530

    make routes

---
+++ Backend/src/routes/admin.Routes.js
@@ -0,0 +1,28 @@
+import express from "express"
+import { Image } from "../controllers/images/index.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { Auth } from "../controllers/Auth/index.js";
+import { verifyJwt } from "../middlewares/verifyJwt.js";
+
+const adminRoutes = express.Router()
+
+adminRoutes.post('/upload',
+
+    verifyJwt,
+    (req, res, next) => {
+        upload.single("file")(req, res, function (err) {
+            if (err) {
+                console.error("Upload error:", err);
+                return res.status(err.statusCode || 400).json({
+                    message: err.message || "Upload failed",
+                });
+            }
+            next();
+        });
+    },
+    Image.Post)
+
+adminRoutes.post('/login' , Auth.Login)
+
+
+export default adminRoutes;
