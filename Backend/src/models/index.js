acbef9efcee22c23be9917c8858a82e62f47c560
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-11 19:29:38 +0530

    update

--- Backend/src/models/index.js
+++ Backend/src/models/index.js
@@ -1,5 +1,9 @@
 import { Admin } from "./slices/admin.Model.js";
+import { Category } from "./slices/category.Model.js";
+import { File } from "./slices/file.Model.js";
 
 export const Model = {
-    Admin
+    Admin,
+    File,
+    Category
 }
