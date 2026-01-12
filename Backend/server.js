8a28617b156c2e5c71478f104b7d8ecf1752f65f
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-12 19:40:42 +0530

    impliment cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,25 @@
 import app from "./index.js"
-
+import cors from "cors"
 // routes
 import adminRoutes from './src/routes/admin.Routes.js'
 import publicRoutes from './src/routes/public.Routes.js'
 
-app.use('/api/v1/admin', adminRoutes)
-app.use('/api/v1/public', publicRoutes)
+const corsOption = {
+    origin: ['http://localhost:5173'],
+    methods: ['GET'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+const privateCorsOptions = {
+    origin: ['https://api-galleryhub.cloudcoderhub.in'],
+    methods: ['GET'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+app.use('/api/v1/admin', cors(privateCorsOptions), adminRoutes)
+app.use('/api/v1/public', cors(corsOption) , publicRoutes)
 
 app.listen(process.env.PORT , '0.0.0.0' , () => {
     console.log(`server is running at port ${process.env.PORT}`)
