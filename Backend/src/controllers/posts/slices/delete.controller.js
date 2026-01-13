1ed75bf8057ada6decaffba00c6df73bc4b910b7
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-13 14:44:49 +0530

    update

---
+++ Backend/src/controllers/posts/slices/delete.controller.js
@@ -0,0 +1,35 @@
+import DevLoad from "devload"
+import { Model } from "../../../models/index.js"
+
+
+export const Delete = async (req, res) => {
+    try {
+        const fileId = req.params.fileId
+
+        const devload = new DevLoad(process.env.DEVLOAD_APIKEY);
+        const file = await Model.File.findById(fileId);
+        if (!file) {
+            return res.status(404).json({
+                message: "Invalid or File Not Found"
+            })
+        }
+
+        const deleteId = file.deleteId
+        for (const key in deleteId) {
+            const value = deleteId[key];
+            if (value) {
+                await devload.deleteFile(id)
+            }
+        }
+
+        await Model.File.deleteOne({ _id: fileId })
+
+        return res.status(200).json({
+            message: "File Deleted Success"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error: "Internal server Error"
+        })
+    }
+}
